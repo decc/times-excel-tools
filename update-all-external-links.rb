1b645c09c8bd037976ffb6600e063951f0b3d0b4
Tom Counsell <tamc-github@greenonblack.com>
2015-06-15 10:06:56 +0000

    Updated update-all-external-links.rb

--- update-all-external-links.rb
+++ update-all-external-links.rb
@@ -2,13 +2,33 @@
 require 'win32ole'
 excel = WIN32OLE.new('Excel.Application')
 dir = File.expand_path(File.dirname(ARGV[0] || '.'))
+number_of_iterations = ARGV[1] || 3
 puts dir
-excel.Visible = 1
-Dir.glob("**/*.xls").each do |workbook|
-	puts workbook
-	name = File.join(dir,workbook).gsub('/','\\')
-	file = excel.Workbooks.Open(name, 0)
-  excel.ActiveWorkbook.UpdateLink( 'Name' => excel.ActiveWorkbook.LinkSources )
-	file.Save
-	file.Close
+excel.Visible = 0
+excel.ScreenUpdating = 0
+number_of_iterations.times do 
+	Dir.glob("**/*.xls*").each do |workbook|
+		puts workbook
+		name = File.join(dir,workbook).gsub('/','\\')
+		next if File.basename(name).start_with?('~')
+		file = excel.Workbooks.Open(name, 0)
+		external_links = excel.ActiveWorkbook.LinkSources 
+		if external_links
+			found_links, missing_links = external_links.partition { |f| File.exist?(f) }
+			unless missing_links.empty?
+				puts "Not updating these links:"
+				puts missing_links
+			end
+			unless found_links.empty?
+				puts "Updating #{found_links.length} external links"
+				excel.ActiveWorkbook.UpdateLink( 'Name' => found_links )
+				excel.Calculate
+				file.Save
+			end
+		end
+		file.Close
+		puts
+	end
 end
+excel.Visible = 1
+excel.ScreenUpdating = 1
