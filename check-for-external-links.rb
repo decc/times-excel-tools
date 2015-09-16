638b477e235290503ae2274c41dc3298f3f0fce6
Philip Sargent <philip.sargent@klebos.com>
2015-09-16 23:54:27 +0100

    Fixed file of links
    File of all links was truncated. Fixed.

--- check-for-external-links.rb
+++ check-for-external-links.rb
@@ -13,36 +13,51 @@
 # in which case it will ONLY show the file:/// and http:// https:// external links. OK.
 # Philip Sargent 2015/09/06
 
-# updated 2015-0915
+# updated 2015-09-16
 
 require 'zip'
 
 wsheets = []
 targets =[]
+links ={}
 
-
+i=0
 Dir.glob("**/*.xls*").each do |workbook|
-name = File.basename(workbook)
-File.open("checks.tsv","w") do |f|
-	next if name.start_with?('~')
-		begin
-			Zip::File.open(workbook) do |spreadsheet|
-				spreadsheet.glob("xl/externalLinks/_rels/*").each do |link|
-					target = link.get_input_stream.read.to_s[/Target="([^"]*)"/,1]
-					if target.start_with?('/') || target =~ /^\w+:/
-						f.puts "#{workbook}\t#{target}"
-						wsheets<<workbook.to_s
-						targets<<target.to_s.gsub(/%20/,' ')
-					end
+fname = File.basename(workbook)
+next if fname.start_with?('~')
+	begin
+		Zip::File.open(workbook) do |spreadsheet|
+			links[workbook.to_s] =[]
+			spreadsheet.glob("xl/externalLinks/_rels/*").each do |link|
+				target = link.get_input_stream.read.to_s[/Target="([^"]*)"/,1]
+				if target.start_with?('/') || target =~ /^\w+:/
+					i += 1
+					#puts "#{i}\t#{workbook.to_s}\t#{target.to_s.gsub(/%20/,' ')}\n"
+					print "\r#{i} links scanned"
+					links[workbook.to_s].push(target.to_s.gsub(/%20/,' '))
+					wsheets<<workbook.to_s
+					targets<<target.to_s.gsub(/%20/,' ')
 				end
 			end
-		rescue Exception => e
-			puts e
+		end
+	rescue Exception => e
+		puts e
+	end
+end
+
+puts "\r#{i} Scanned links. They are listed in check-links.tsv"
+
+File.open("check-links.tsv","w") do |ff|
+	links.each do |w,tl|
+		tl.each do |l|
+			ff.puts "#{w}\t#{l}"
 		end
 	end
 end
-puts "#{wsheets.uniq!.count} files with references. They are listed in check-files-external-links.tsv"
-File.open("check-files-external-links.tsv","w") do |f|
+
+
+puts "#{wsheets.uniq!.count} files with references. They are listed in check-files-external-links.txt"
+File.open("check-files-external-links.txt","w") do |f|
   wsheets.sort!
   f.puts wsheets
 end
@@ -51,3 +66,4 @@ File.open("unique-check.txt","w") do |f|
   targets.sort!.uniq!
   f.puts targets
 end
+puts "#{targets.count} Referenced files. They are listed in unique-check.txt"
