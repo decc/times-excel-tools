00933e566abf537bbf90bb7e7e6c4f5bea44b602
Philip Sargent <philip.sargent@klebos.com>
2015-09-17 14:40:43 +0100

    Now does topo sort with fallbacks for loops
    Where there is a set of files in a loop, this set is repeated as many
    times as there are members. This repeated set is inserted in the correct
    place of the topological sorted list.

--- explore-links.rb
+++ explore-links.rb
@@ -7,13 +7,16 @@
 
 # As we are interested in the link graph of local files only, EXCLUDE all the file:/// and http:// links.
 
+# Still need to check that base filenames are globally unique !
+# Still need to sort out proper paths for export list of sorted filenames
+# Still need to deal with links to files which do not actually exist.
+
 # Originally written by Philip Sargent 2015 09 06
-# updated 2015-09-15 PMS
+# updated 2015-09-17 PMS
 
 require 'zip' # xlsx files are zip files full of xml
 require 'pathname' # Makes manipulating paths easier
 require 'uri' # The links are encoded as urls
-require 'digest'
 require 'tsort'
 
 class TsortableHash < Hash
@@ -42,8 +45,6 @@ i=0
 # Hence the number of files seen does not match those searched for using *.xls* 
 
 Dir.glob("**/*.xls*").each do |workbook|
-	cksums[workbook] = Digest::SHA2.file(workbook).hexdigest 
-
 	name = File.basename(workbook)
 	i=i+1
 	print "\r#{i}\tworkbooks scanned "
@@ -68,8 +69,12 @@ Dir.glob("**/*.xls*").each do |workbook|
 				unless t =~ /^\w+:/ # keeps http:// and file:// targets as full pathnames but DO NOT add to graph
 					parents << parent
 					tb = File.basename(t, '.*').downcase
+					#if fullpaths[tb].nil?
+					#	fullpaths[tb] = t.gsub(/%20/,' ').gsub!('/','\\')
+					#end
 					targets << tb
 					arcs[parent] = targets
+					
 				end						
 			end
 		end
@@ -79,12 +84,6 @@ Dir.glob("**/*.xls*").each do |workbook|
 	end
 end
 puts "\n"
-
-File.open("cksums.tsv","w") do |f|
-	cksums.each do |w,ck|
-		f.puts "#{w}\t#{ck}"
-	end
-end
 
 parents.sort!.uniq!
 puts "#{parents.count}\tworkbooks have links to other local files."
@@ -100,6 +99,17 @@ arcs.each do |p,t|
 	end
 end
 
+alltargets.each do |t| 
+	if fullpaths[t].nil?
+		puts "\nMISSING Full path for this workbook: #{t}"
+	else
+		unless File.exist?(fullpaths[t])
+			puts "\nMISSING FILE: #{t} #{fullpaths[t]}"
+		end
+	end
+end
+		
+		
 #WHY is this not sorting !??!
 newt = alltargets.sort.uniq
 puts "#{newt.count}\tlocal files are targets of these links."
@@ -138,14 +148,17 @@ newarcs.each do |a|
 	arcs[a]=[]
 end
 
-puts "\nAll file-to-file links:"
-arcs.each do |p,t|
-	unless t.empty? 
-		puts "\n#{p}"
-		puts "#{t}"
+puts "\nAll file-to-file links in dependencies.txt"
+File.open("dependencies.txt","w") do |f|
+	arcs.each do |p,t|
+		unless t.empty? 
+			f.puts "\n#{p}"
+			f.puts "#{t}"
+		end
 	end
 end
 
+cyclics = {}
 # Using the Tsort methods will expose cyclic depdendency
 begin # start exception block
 	nl=0
@@ -154,6 +167,7 @@ begin # start exception block
 	arcs.each_strongly_connected_component do |scc| # iterates through arrays of nodes
 		if scc.length > 1 then 
 			nl = nl +1 
+			cyclics[nl]=scc
 			puts "\nCyclic loop #{nl}:"
 			scc.each do |g|
 				printf "   %-40s %s\n",  g, fullpaths[g]
@@ -162,16 +176,139 @@ begin # start exception block
 		#puts "parent:#{p}"
 		#puts "\ttargets: #{t}"
 	end
-	puts "\nTopological sort:"
-	puts arcs.tsort
+	# puts "\nTopological sort:"
+	# puts arcs.tsort
 rescue Exception => e
-	puts "\nEXCEPTION\n"
+	puts "\nEXCEPTION Begin\n"
 	puts e
+	puts "\nEXCEPTION End\n"
+end
+
+listids ={}
+# Now compact each loop into one node, and produce a clean topo sort.
+# First create new parent nodes
+cyclics.each do |nl,scc|
+	id="~SCC-" + nl.to_s
+	listids[nl] = id.to_s
+	arcs[id] = [] # create new node for the compactified scc
+	scc.each do |p|
+		# puts id, p, "->",arcs[p]
+		
+		# copy target lists of all loop members to new parent
+		tl = arcs[id] + arcs[p]
+		arcs[id] = tl
+		arcs.delete(p)
+		
+		# Next replace all targets with the loop names
+		arcs.each do |pp,_|
+			if arcs[pp].include?(p)
+				#puts "replace #{pp} - with link to #{id}"
+				arcs[pp].delete(p)
+				arcs[pp] << id
+			end
+			
+		
+		end
+	end
+	#puts "\n#{id} -> #{arcs[id]}"
+	
+
+
 end
 
 
+
+# Next remove duplicates in target lists 
+arcs.each do |p,t|
+	targets = arcs[p]
+	arcs[p] =[]
+	arcs[p] = targets.uniq
+end
+
+cyclics2 = {}
+topolist = []
+# Using the Tsort methods will expose cyclic depdendency
+begin # start exception block
+	nl=0
+	puts "\nTry again having compacted the loops \n(aka Strongly connected components aka cyclic dependencies with more than one member):"
+	# arcs.each_strongly_connected_component {|scc| p scc }
+	arcs.each_strongly_connected_component do |scc| # iterates through arrays of nodes
+		if scc.length > 1 then 
+			nl = nl +1 
+			cyclics2[nl]=scc
+			puts "\nCyclic loop #{nl}:"
+			scc.each do |g|
+				printf "   %-40s %s\n",  g, fullpaths[g]
+			end
+		end
+		#puts "parent:#{p}"
+		#puts "\ttargets: #{t}"
+	end
+	#puts "\nTopological sort:"
+	topolist = arcs.tsort
+	#puts "#{topolist}"
+rescue Exception => e
+	puts "\nEXCEPTION Begin\n"
+	puts e
+	puts "\nEXCEPTION End\n"
+end
+
+
+#puts "\nNow replace the SCC items"
+#Now replace each SCC with the real files, but duplicate by the size of the SCC
+
+# First prepare the replacements for ~SCC-1 etc
+fscc = {}
+
+cyclics.each do |nl, _|
+	fscc[listids[nl]]=[]
+	#puts "\n#{listids[nl]}"
+	cyclics[nl].each do |_| # repeat n times for cyclic loop of size n
+		fscc[listids[nl]].push(cyclics[nl])
+	end
+	#puts fscc[listids[nl]]
+end
+
+#puts "#{topolist}\n"
+#puts "\n"
+#puts listids
+#puts "\nNow replace SCCs with multiplied original workbook names"
+newtopo = []
+
+substitutes=[]
+topolist.each do |t|
+	found=false
+	listids.each do |nl, _|
+		if listids[nl].match(t)
+			found=true
+			substitutes = fscc[listids[nl]]
+		end
+	end
+	
+	if found 
+		#puts "Found #{t}"
+		newtopo.push(substitutes.flatten)
+		#puts "Replacing with #{substitutes}"
+		#puts "#{newtopo}\n"
+		next
+	else
+		newtopo << t
+		#puts "Adding #{t}"
+	end
+end
+#puts "#{newtopo}\n"
+	
+newtopo.flatten!
+
+puts "\nTopological sorted filelist in topolist.tsv"
+File.open("topolist.tsv","w") do |f|
+	newtopo.each do |n|
+		f.puts "#{n}\t#{fullpaths[n]}"
+	end
+end
+
 # temporary files skipped
-puts "\nNOTE that these files have been ignored:"
+puts "\nNOTE that these files have been ignored (either .xlsM or ~temporaries):"
 skips.each do |s|
 	puts "\t#{s}"
 end
